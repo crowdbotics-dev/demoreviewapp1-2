358918ebcf607112a5c332619666fa7e511e9319
Aline Tavares <aline@crowdbotics.com>
2022-11-11 11:45:15 -0500

    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,12 +1,10 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
   name: "demoreviewapp1-2",
   url: "https://demoreviewapp1-2.botics.co",
   api: "https://demoreviewapp1-2.botics.co/api/v1"
-}
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
---
+++ screens/Untitled1/index.js
@@ -0,0 +1,83 @@
+import React from "react";
+import { View, StyleSheet } from "react-native";
+
+const Untitled1 = () => {
+  return <View style={_styles.mzmdqLNQ}>
+      </View>;
+};
+
+export default Untitled1;
+
+const _styles = StyleSheet.create({
+  mzmdqLNQ: {
+    backgroundColor: "#f0f0f1",
+    padding: 10,
+    position: "relative",
+    height: "100%"
+  },
+  xPAvgQEV: {
+    backgroundColor: "#fff",
+    elevation: 15,
+    shadowColor: "#ccc9c9",
+    marginBottom: 20,
+    borderRadius: 8
+  },
+  bBiHKqoQ: {
+    backgroundColor: "#fff",
+    padding: 10,
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    marginHorizontal: 5
+  },
+  CvjXqvfT: {
+    justifyContent: "space-evenly",
+    alignItems: "flex-start"
+  },
+  kMzkGzDU: {
+    fontSize: 13,
+    color: "#000",
+    paddingTop: 10
+  },
+  bgRuqgsg: {
+    fontSize: 13,
+    color: "#000",
+    paddingTop: 10
+  },
+  SkVomqYD: {
+    fontSize: 13,
+    color: "#000",
+    paddingTop: 10
+  },
+  HhkAUtxy: {
+    fontSize: 13,
+    color: "#000",
+    paddingTop: 10
+  },
+  kqAdFJMP: {
+    justifyContent: "space-evenly",
+    alignItems: "flex-end"
+  },
+  CTzMVosr: {
+    fontSize: 13,
+    color: "#000",
+    paddingTop: 10
+  },
+  WxHEtLWH: {
+    fontSize: 13,
+    color: "#000",
+    paddingTop: 10
+  },
+  OnERyPcV: {
+    fontSize: 13,
+    color: "#000",
+    paddingTop: 10
+  },
+  xeIPcRjc: {
+    width: 20.4,
+    height: 14.4,
+    resizeMode: "contain",
+    marginTop: 10
+  }
+});
